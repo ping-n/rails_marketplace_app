2b6809e0ea8140f8a9561194bbfd998edfa1dc5c
PING NGE <ping.nge@hotmail.com>
2020-05-12 15:00:40 +1000

    starting seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require_relative 'data/data'
+
+#Cleaning up Data-base
+Category.destroy_all
+Listing.destroy_all
+User.destroy_all
+
+
+puts 'Seeding Users'
+p "--" * 17
+user = User.create(email: 'admin@mail.com', password: 'password')
+puts "Created User 1"
+user2 = User.create(email: 'test@mail.com', password: 'password')
+puts "Created User 2"
+p "--" * 17
+puts 'Successfully Seeded Users ✅'
+
