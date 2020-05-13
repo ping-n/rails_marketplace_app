3c975420fb926f194ae01e61c6979d82603af0e0
PING NGE <ping.nge@hotmail.com>
2020-05-13 13:36:49 +1000

    edited user model to include roles and edited seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :listings, dependent: :destroy
 
+  def set_default_role
+    self.role ||= :user
+  end
 end
