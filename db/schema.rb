d3f5823b743938e8e98c906a1ad1679277b6cb57
PING NGE <ping.nge@hotmail.com>
2020-05-15 00:23:09 +1000

    added default false to sold in listings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_050200) do
+ActiveRecord::Schema.define(version: 2020_05_14_141322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2020_05_13_050200) do
     t.string "brand"
     t.decimal "price"
     t.integer "condition"
-    t.boolean "sold"
+    t.boolean "sold", default: false
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
