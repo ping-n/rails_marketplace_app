2fc5224594428edf8bce8126fb40632c33479289
PING NGE <ping.nge@hotmail.com>
2020-05-14 13:50:38 +1000

    edting cards for index pages and show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,9 @@ import '../stylesheets/application'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+// sidenav code
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
