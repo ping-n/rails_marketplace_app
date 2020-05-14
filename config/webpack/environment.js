0a9a7932c9b4441e2d05bc0d8d824936cc905d87
PING NGE <ping.nge@hotmail.com>
2020-05-14 10:10:57 +1000

    removed bootstrap, installed materialize

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,4 @@
 
  const webpack = require('webpack')
 
- environment.plugins.append("Provide", new webpack.ProvidePlugin({
-   $: 'jquery',
-   jQuery: 'jquery',
-   Popper: ['popper.js', 'default']
- }))
-
  module.exports = environment
