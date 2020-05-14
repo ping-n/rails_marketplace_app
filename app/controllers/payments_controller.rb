3b1f8b5bebbe6b3f8fcb6463b2ea76574590ffd2
PING NGE <ping.nge@hotmail.com>
2020-05-15 00:26:07 +1000

    fixing rubocop error

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:webhook]
   def stripe_id
     @listing = Listing.find(params[:id])
     session_id = Stripe::Checkout::Session.create(
@@ -24,5 +25,19 @@ class PaymentsController < ApplicationController
 
   def success
     render payments_success_path
+  end
+
+  def webhook
+    payment_id = params[:data][:object][:payment_intent]
+    payment = Stripe::PaymentIntent.retrieve(payment_id)
+    listing_id = payment.metadata.listing_id
+    user_id = payment.metadata.user_id
+    listing = Listing.find(listing_id)
+    listing.sold = true
+    listing.save
+    Rails.logger.debug 'listing id ' + listing_id
+    Rails.logger.debug 'user id ' + user_id
+
+    head 200
   end
 end
