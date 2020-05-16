eb64e7ce5e778b79baa4fe4a283324d3db545713
PING NGE <ping.nge@hotmail.com>
2020-05-16 18:27:43 +1000

    forgot to remove raise in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -16,7 +16,6 @@ class CartsController < ApplicationController
            else
              current_user.cart
            end
-    raise
     listing = Listing.find(params[:listing_id])
     cart.listing_carts.create(listing: listing)
     redirect_to listings_path
@@ -26,7 +25,7 @@ class CartsController < ApplicationController
     cart = current_user.cart
     @product = cart.listing_carts.find_by(listing_id: params[:listing_id]).id
     ListingCart.destroy(@product)
-    flash[:success] = "removed from cart"
+    flash[:success] = 'removed from cart'
     redirect_to carts_path
-  end 
+  end
 end
