701111d15136f972d5b35a55b53c27b7ed144e62
PING NGE <ping.nge@hotmail.com>
2020-05-17 12:08:34 +1000

    added route for profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
   resources :listings
   resources :carts, only: [:create, :index]
   devise_for :users
+  resources :profiles, only: [:show, :update, :edit]
   delete "/cart", to: "carts#destroy", as: "destroy_cart"
-  get "/payments/session", to: 'payments#stripe_id'
+  get "/payments/session", to: "payments#stripe_id"
   get "/payments/success", to: "payments#success"
   post "/payments/webhook", to: "payments#webhook"
   root to: "pages#index"
