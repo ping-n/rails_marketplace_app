2505c27c5459515f676763a3b8c1e54bb45befdb
PING NGE <ping.nge@hotmail.com>
2020-05-17 16:49:10 +1000

    added dashboard page and baisic layout

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,2 +1,14 @@
 class DashboardsController < ApplicationController
+  before_action :authenticate_user!
+  def show
+    user = User.find(params[:id])
+    @listings = user.listings
+    @profile = user.profile
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
