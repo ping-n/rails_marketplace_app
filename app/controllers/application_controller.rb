e6cf7d4bb6767a7eb8cf4523f9a7aa5583dfad0c
PING NGE <ping.nge@hotmail.com>
2020-05-17 22:31:55 +1000

    added search bar, need to style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_query
   rescue_from CanCan::AccessDenied do |_exception|
     render file: Rails.root.join('public/403.html'), status: :forbidden
+  end
+
+  def set_query
+    @q = Listing.ransack(params[:q])
   end
 end
