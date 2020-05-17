d540514b0df1eafb3d141f61eb3a85beb222aead
PING NGE <ping.nge@hotmail.com>
2020-05-17 12:10:21 +1000

    added show edit and update to profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,30 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: %i[show edit update]
+  before_action :authenticate_user!
+  load_and_authorize_resource
+  def show; end
+
+  def edit; end
+
+  def update
+    @profile.user_info.update(
+      profile_params
+    )
+    redirect_to
+  end
+
+  private
+
+  def profile_params
+    params.require(:user_info).permit(
+      :first_name,
+      :surname,
+      :location,
+      :user_id
+    )
+  end
+
+  def set_profile
+    @profile = current_user
+  end
 end
