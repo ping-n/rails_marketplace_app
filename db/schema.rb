7c85889aa41e83ebfe0d6c364b1c56df02a0e4e4
PING NGE <ping.nge@hotmail.com>
2020-05-20 11:59:19 +1000

    added description migration to profile model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_020236) do
+ActiveRecord::Schema.define(version: 2020_05_20_012314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_05_18_020236) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
