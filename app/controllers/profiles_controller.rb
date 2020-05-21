6b98456c55e612975a7ef7125de5c4857f5b8b8f
PING NGE <ping.nge@hotmail.com>
2020-05-21 18:48:59 +1000

    fixed profile form so that errors messages show up

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,5 @@
 class ProfilesController < ApplicationController
-  before_action :set_profile, only: %i[show edit update]
+  before_action :set_profile, only: %i[show edit]
   before_action :authenticate_user!
   load_and_authorize_resource
 
@@ -8,22 +8,33 @@ class ProfilesController < ApplicationController
   def edit; end
 
   def update
-    @user.profile.update(
-      profile_params
-    )
-    redirect_to profile_path
+    @user = current_user.profile
+    scope = nil
+    scope = :user if profile_params.key?(:user)
+    scope = :profile if profile_params.key?(:profile)
+    if @user.update(profile_params[scope])
+      redirect_to dashboard_path
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def profile_params
-    params.require(:profile).permit(
-      :first_name,
-      :surname,
-      :location,
-      :user_id,
-      :description
-    )
+    params.permit(user: %i[
+                    first_name
+                    surname
+                    location
+                    user_id
+                    description
+                  ], profile: %i[
+                    first_name
+                    surname
+                    location
+                    user_id
+                    description
+                  ])
   end
 
   def set_profile
