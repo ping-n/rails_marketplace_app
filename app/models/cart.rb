251c08364439c02906693a1ac10f6cf43d2f87ef
PING NGE <ping.nge@hotmail.com>
2020-05-21 11:11:04 +1000

    added dependent destroy, so it allow user to delete their account

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many :listing_carts
-  has_many :listings, through: :listing_carts
+  has_many :listings, through: :listing_carts, dependent: :destroy
 end
